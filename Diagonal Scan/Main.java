e3e0e45c54aa4e4306f91f767a9de530b439fd29
ryonath <61133197+bushheadryo@users.noreply.github.com>
2023-05-30 14:06:02 -0700

    Update Main.java

--- Diagonal Scan/Main.java
+++ Diagonal Scan/Main.java
@@ -1,3 +1,7 @@
+/**
+* For any N x M board, find the number of times a diagonal of length L appears in the board
+**/
+
 public class Main {
     static int num (String[][] board, int l, String s) {
         int x; int y = board.length - l; int num = 0;
